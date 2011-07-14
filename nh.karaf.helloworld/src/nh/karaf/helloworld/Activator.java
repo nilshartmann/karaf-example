66e9686dd8430058130031ca249677fe2317f841
Nils Hartmann <nils@nilshartmann.net>
2011-07-14 21:51:18 +0200

    initial commit

---
+++ nh.karaf.helloworld/src/nh/karaf/helloworld/Activator.java
@@ -0,0 +1,35 @@
+package nh.karaf.helloworld;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		
+		System.out.println("Hallo, Welt !!!");
+		
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+		
+		System.out.println("Goodbye, Welt !!!");
+	}
+
+}
